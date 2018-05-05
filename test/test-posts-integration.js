2cebb92a24ca24e0289d45a4b42ebc6b16581df6
mike crane <mike.c0321@gmail.com>
2018-05-04 23:01:47 -0400

    set up tests for POST, PUT and DELETE requests

--- test/test-posts-integration.js
+++ test/test-posts-integration.js
@@ -109,7 +109,88 @@ describe('Blog posts API resource', function() {
           expect(resBlogPost.id).to.equal(post.id);
           expect(resBlogPost.title).to.equal(post.title);
           expect(resBlogPost.content).to.equal(post.content);
-          expect(resBlogPost.author).to.contain(post.author.lastName);
+          // expect(resBlogPost.author).to.contain(post.author.lastName);
+        });
+    });
+  });
+
+  //post request test
+  describe('POST endpoint', function () {
+    it('should add a new blog post', function () {
+
+      const newBlogPost = generateBlogPostData();
+
+      return chai.request(app)
+        .post('/posts')
+        .send(newBlogPost)
+        .then(function (res) {
+          expect(res).to.have.status(201);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.include.keys(
+            'id', 'title', 'content', 'author');
+          expect(res.body.id).to.not.be.null;
+          expect(res.body.title).to.equal(newBlogPost.title);
+          expect(res.body.content).to.equal(newBlogPost.content);
+          // expect(res.body.author).to.equal(newBlogPost.author.firstName);
+
+          return BlogPost.findById(res.body.id);
+        })
+        .then(function (post) {
+          expect(post.title).to.equal(newBlogPost.title);
+          expect(post.content).to.equal(newBlogPost.content);
+          // expect(post.author.firstName).to.equal(newBlogPost.author.firstName);
+        });
+    });
+  });
+
+  // put request test
+  describe('PUT endpoint', function () {
+    it('should update fields you send over', function () {
+      const updateData = {
+        title: 'Mr. Bigglesworth goes to Washington',
+        content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.'
+      };
+
+      return BlogPost
+        .findOne()
+        .then(function (post) {
+          updateData.id = post.id;
+
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+        })
+        .then(function (res) {
+          expect(res).to.have.status(204);
+
+          return BlogPost.findById(updateData.id);
+        })
+        .then(function (post) {
+          expect(post.title).to.equal(updateData.title);
+          expect(post.content).to.equal(updateData.content);
+        });
+    });
+  });
+
+  // delete request test
+  describe('DELETE endpoint', function () {
+    it('delete a post by id', function () {
+
+      let post;
+
+      return BlogPost
+        .findOne()
+        .then(function (_post) {
+          post = _post;
+          return chai.request(app).delete(`/posts/${post.id}`);
+        })
+        .then(function (res) {
+          expect(res).to.have.status(204);
+          return BlogPost.findById(post.id);
+        })
+        .then(function (_post) {
+          expect(_post).to.be.null;
         });
     });
   });
